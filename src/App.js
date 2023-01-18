2d50c0bfd13ddc7f58d3de788ab862c64cd239a0
Federico Suarez <fsuarez@intermediait.com>
2023-01-18 15:56:38 -0300

    add tailwind and layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.jpg'
 import './App.css'
+import { Layout } from './components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="https://linktr.ee/sondelsur">
-          <img src={logo} alt="logo" className='App-logo' />
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <div className="App">
+        <header className="App-header">
+          <a href="https://linktr.ee/sondelsur">
+            <img src={logo} alt="logo" className="App-logo" />
+          </a>
+        </header>
+      </div>
+    </Layout>
   )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,66 @@
+import logo from '../logo.jpg'
+
+export const Header = () => {
+  return (
+    <div class="min-h-full">
+      <nav class="bg-gray-800">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div class="flex h-16 items-center justify-between">
+              <div class="flex-shrink-0">
+                <img class="h-8 w-8" src={logo} alt="sondelsur" />
+              </div>
+              <div class="hidden md:block content-end">
+                <div class="ml-12 flex items-baseline space-x-4">
+                  <button
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium"
+                    aria-current="page"
+                  >
+                    Sobre Nosotros
+                  </button>
+
+                  <button class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                    Servicios
+                  </button>
+
+                  <button class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                    Material
+                  </button>
+
+                  <button class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                    Contacto
+                  </button>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="md:hidden" id="mobile-menu">
+          <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+            <button
+              class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+              aria-current="page"
+            >
+              Sobre Nosotros
+            </button>
+
+            <button class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Servicios
+            </button>
+
+            <button class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Material
+            </button>
+
+            <button class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Calendar
+            </button>
+
+            <button class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+              Contacto
+            </button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
