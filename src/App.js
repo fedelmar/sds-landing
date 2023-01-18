3f25c724373844c8388b29f22f526cee8402e392
Federico Suarez <fsuarez@intermediait.com>
2023-01-18 17:40:32 -0300

    header changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.jpg'
+import logo from './images/logo.jpg'
 import './App.css'
 import { Layout } from './components'
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,38 @@
-import logo from '../logo.jpg'
-
 export const Header = () => {
   return (
     <div class="min-h-full">
       <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div class="flex h-16 items-center justify-between">
-              <div class="flex-shrink-0">
-                <img class="h-8 w-8" src={logo} alt="sondelsur" />
+            <h1 className="text-white">Son del Sur</h1>
+            <div class="hidden md:block content-end">
+              <div class="ml-12 flex items-baseline space-x-4">
+                <button
+                  class="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium"
+                  aria-current="page"
+                >
+                  Inicio
+                </button>
+
+                <button
+                  class="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium"
+                  aria-current="page"
+                >
+                  Sobre Nosotros
+                </button>
+
+                <button class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Material
+                </button>
+
+                <button class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Servicios
+                </button>
+
+                <button class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Contacto
+                </button>
               </div>
-              <div class="hidden md:block content-end">
-                <div class="ml-12 flex items-baseline space-x-4">
-                  <button
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium"
-                    aria-current="page"
-                  >
-                    Sobre Nosotros
-                  </button>
-
-                  <button class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Servicios
-                  </button>
-
-                  <button class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Material
-                  </button>
-
-                  <button class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Contacto
-                  </button>
-                </div>
             </div>
           </div>
         </div>
@@ -40,15 +43,22 @@ export const Header = () => {
               class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
               aria-current="page"
             >
+              Inicio
+            </button>
+
+            <button
+              class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+              aria-current="page"
+            >
               Sobre Nosotros
             </button>
 
             <button class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-              Servicios
+              Material
             </button>
 
             <button class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-              Material
+              Servicios
             </button>
 
             <button class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
