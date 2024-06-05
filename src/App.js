e081b9c00074b8535e1140c769563554e1a162df
Federico Suarez <fsuarez91@gmail.com>
2024-06-05 15:47:01 -0300

    add title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Son del Sur</h1>
         <a href="https://linktr.ee/sondelsur">
           <img src={logo} alt="logo" className='App-logo' />
         </a>
